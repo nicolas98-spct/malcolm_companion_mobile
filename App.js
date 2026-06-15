d673e2d94ef934c04363cfc8c81225ca704e3f99
Nicolás <nicolas98223@gmail.com>
2026-06-14 23:06:03 -0500

    Stabilize Expo Nativewind configuration

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+﻿import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { RootNavigator } from './src/navigation';
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
-/** @type {import('tailwindcss').Config} */
+﻿/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./index.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
-    extend: {
-      colors: {
-        malcolm: { red: '#d71920', yellow: '#ffc928', black: '#101010', gray: '#f1f1f1' },
-      },
-      fontFamily: {
-        body: ['NeueHaasGrotesk'],
-        title: ['HelveticaNeueBold'],
-      },
-    },
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 };
